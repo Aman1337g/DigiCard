67f5b6a4ab84676afe7d070f96ce53555e87e1a9
ABISHEK <abishekupadhyay2020@gmail.com>
2025-05-08 00:12:52 +0530

    Fixed breaking reloads for routes

--- src/components/Admin/index.jsx
+++ src/components/Admin/index.jsx
@@ -39,6 +39,7 @@ const Admin = () => {
 
   // Colors for charts
   const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "#8884D8"];
+  console.log(offenders)
 
   useEffect(() => {
     const fetchData = async () => {
